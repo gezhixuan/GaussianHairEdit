99ef86637a77afeb852e60230f7e02fdd9a42d8e
gezhixuan <1051408504@qq.com>
2025-11-05 15:48:55 -0600

    preprocrdding :wq

--- script/preprocess.sh
+++ script/preprocess.sh
@@ -1,5 +1,8 @@
 # ID="Zhixuan"
-ID="Liza"
+# ID="Zhixuan_3"
+# ID="Liza"
+ID="Liza_3"
+
 export PROJECT_DIR="/scratch/hl106/zx_workspace/cto/VcEdit"
 export DATA_PATH="$PROJECT_DIR/gs_data/$ID"
 export CAMERA="PINHOLE"
@@ -11,18 +14,18 @@ cd "$DATA_PATH"
 eval "$(conda shell.bash hook)"
 
 
-ffmpeg -noautorotate -i ./merged.mp4 \
-  -vf "transpose=1,setsar=1" \
-  -metadata:s:v:0 rotate=0 -map_metadata -1 \
-  -c:v libx264 -crf 18 -preset slow -pix_fmt yuv420p \
-  -c:a copy ./raw_no90.mp4
+# ffmpeg -noautorotate -i ./merged.mp4 \
+#   -vf "transpose=1,setsar=1" \
+#   -metadata:s:v:0 rotate=0 -map_metadata -1 \
+#   -c:v libx264 -crf 18 -preset slow -pix_fmt yuv420p \
+#   -c:a copy ./raw_no90.mp4
 
-ffmpeg -i ./raw_no90.mp4 -c copy -metadata:s:v:0 rotate=0 ./raw.mp4
+# ffmpeg -i ./raw_no90.mp4 -c copy -metadata:s:v:0 rotate=0 ./raw.mp4
 
 conda deactivate && conda activate vcedit
 cd $PROJECT_DIR/src/preprocessing
 CUDA_VISIBLE_DEVICES="$GPU" python preprocess_raw_images.py \
-    --data_path $DATA_PATH
+    --data_path $DATA_PATH --target_fps 3
 
 conda deactivate && conda activate vcedit
 cd $PROJECT_DIR/src
